7d0ab47ad7da29dfef89919c79f701e6d316e6c9
Dianashafee <diana.shafee1997@hotmail.com>
2020-12-04 21:42:18 +0200

    tests were added

--- madlib_cli/madlib_cli.py
+++ madlib_cli/madlib_cli.py
@@ -6,11 +6,11 @@ def welcoming():
     print(f"""
     *************************************************
 
-     Welcome in the Madlib Game ....
-     Are you Ready to have fun with me ? 
-     I will be asking you to guess things about me  
-     and be prepared to laugh hard
-     LET'S GO ....
+            Welcome in the Madlib Game ....
+            Are you Ready to have fun with me ? 
+            I will be asking you to guess things about me  
+            and be prepared to laugh hard
+            LET'S GO ....
 
     *************************************************
     """)
@@ -21,14 +21,12 @@ welcoming()
 def read(path):
     with open(path,'r') as file:
      content = file.read()
-    return (content)
+    return content
 
 read('assets/madlib_source.txt')
 
 
-
-
-def merge(path):
+def questions():
     questions=['Somthing i like doing...',"Another thing i love ....","Guess My major  ....",
     "Enter a verb ends with ing :P ....","Write another verb my dear ^.^  ...."
     ]
@@ -37,18 +35,20 @@ def merge(path):
     for i in questions:
         question= input(i)
         values.append(question)
-    dataText = read(path)
-    answer=dataText.format(*values)   
+    return values
+
+def merge(text,answers):
+    dataText = text
+    answer=dataText.format(*answers)
+    print(answer)   # returning the input that were answered by the user using formating the whole text, and replace it with the appended words (answers)
     return answer
+
 with open('assets/madlib_result.txt', 'w') as file:
-    file.write(merge('assets/madlib_source.txt'))
-
-
-print(merge('assets/madlib_source.txt'))
-
+    file.write(merge(read('assets/madlib_source.txt'),questions()))
+# print(merge('assets/madlib_source.txt',questions()
 
 def parse(path):
-    data=re.findall(r"\{(.*?)\}",read(path))
+    data=re.findall(r"\{(.*?)\}",read(path)) #finds all words between {}, and send then in a list 
     return data
 print(parse('assets/madlib_source.txt'))    
 
--- tests/test_madlib_cli.py
+++ tests/test_madlib_cli.py
@@ -1,14 +1,26 @@
 
 from madlib_cli import __version__
-# from madlib_cli.madlib_cli import *
+from madlib_cli.madlib_cli import *
 
 def test_version():
     assert __version__ == '0.1.0'
 
 
+def test_read():
+    actual = read('assets/madlib_source.txt')
+    expected = open('assets/madlib_source.txt','r').read()
+    assert expected == actual 
+
+def test_parse():
+    actual = parse('assets/madlib_source.txt')
+    expected = ['0','1', '2', '3', '4']
+    assert expected == actual 
 
 
-
+def test_merge():
+    actual = merge('Guess things about me  ^.^! \n My name is Diana i love {0} and {1}, However i am studying {2} \n but you know i can never stop {3}, and i always {4} Big ^.^', ['singing', 'painting', 'translation', 'dreaming', 'dream'])
+    expected = 'Guess things about me  ^.^! \n My name is Diana i love singing and painting, However i am studying translation \n but you know i can never stop dreaming, and i always dream Big ^.^'
+    assert expected == actual
 
 
 
